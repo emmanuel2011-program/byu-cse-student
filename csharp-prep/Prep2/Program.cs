ee2e68c2d5f28cc3d986b70ef7a47235c78f2a0d
emmanuel2011 <okoyee61@gmail.com>
2024-09-22 18:04:42 +0100

    added formulars

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -1,9 +1,68 @@
 using System;
+using System.Runtime.CompilerServices;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.Write("What is your grade percentage? ");
+        string answer = Console.ReadLine();
+        int percent = int.Parse(answer);
+        string letter = "";
+
+
+        string sign = "";
+
+        int lastdigit = percent % 10;
+        
+        if (percent >=90)
+        {     letter = "A";
+       
+        }
+        else if (percent >=80)
+        {   letter = "B";
+        }
+
+        else if (percent >=70)
+        {   letter = "C";
+        }
+        else if (percent >=60)
+        {   letter = "D";
+        }
+
+        else {
+            letter ="F";
+        }
+        if (lastdigit >=7)
+        {   sign = "+";
+        }
+        else if (lastdigit <=3)
+        {   sign = "-";
+        }
+        else {
+            sign = "";
+        }
+        if (lastdigit >=93)
+            {sign = "";
+            
+            }
+
+            if (letter == "F")
+             {    sign = "";
+                }
+        Console.WriteLine($"Your grade is: {letter}{sign}");
+
+        if (percent >=70)
+        {   Console.WriteLine("passed");
+        }
+        else {
+            Console.WriteLine("better luck next time");
+        }
+
+       
     }
+
+
+
+    
 }
