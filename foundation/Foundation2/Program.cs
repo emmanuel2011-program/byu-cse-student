7b9ab84fd8066ad4ad4a18b39b450aaab926faeb
emmanuel2011 <okoyee61@gmail.com>
2024-10-19 10:16:31 +0100

    items added

--- foundation/Foundation2/Program.cs
+++ foundation/Foundation2/Program.cs
@@ -5,8 +5,8 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Foundation2 World!");
-        {
-        // 1 customer
+
+        // Customer 1
         Customer customer1 = new();
         Product customer1Product1 = new();
         Product customer1Product2 = new();
@@ -14,7 +14,7 @@ class Program
         Order order1 = new();
         //////////////////////////////////////////////
         customer1.SetCustomerName();
-        customer1.SetCustomerAddress("23 Ghana Street offf Aminu Kano Wuse 2", "Abuja", "FCT", "Nigeria");
+        customer1.SetCustomerAddress("7G Damba Road, MurrayTown", "Freetown", "Western Area", "Sierra Leone");
         order1.AddCustomer(customer1);
         //////////////////////////////////////////////
         customer1Product1.SetProductName();
@@ -37,7 +37,7 @@ class Program
         order1.DisplayShippingInfo();
 
 
-        // 2nd customer
+        // Customer 2
         Customer customer2 = new();
         Product customer2Product1 = new();
         Product customer2Product2 = new();
@@ -45,7 +45,7 @@ class Program
         Order order2 = new();
         //////////////////////////////////////////////
         customer2.SetCustomerName();
-        customer2.SetCustomerAddress("3rd Avenue Gwarimpa", "Abuja", "Abuja", "Nigeria");
+        customer2.SetCustomerAddress("177 Ave, Iverton", "New Jersey", "New Jersey", "USA");
         order2.AddCustomer(customer2);
         //////////////////////////////////////////////
         customer2Product1.SetProductName();
@@ -59,21 +59,13 @@ class Program
         customer2Product2.SetQuantity();
         order2.AddProduct(customer2Product2);
         Console.WriteLine("");
-        //////////////////////////////////////////////
+        ///////////////////////////////////////////////
         customer2Product3.SetProductName();
         customer2Product3.SetPrice();
         customer2Product3.SetQuantity();
         order2.AddProduct(customer2Product3);
         Console.WriteLine("");
         order2.DisplayShippingInfo();
-
-    }
-
-
-
-    
-
-
 
     }
 }
